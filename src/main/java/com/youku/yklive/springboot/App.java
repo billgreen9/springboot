a35b8e601c717a154b056179af5fa1fe0bdec505
liuquan <liuquan@youku.com>
2016-09-07 11:32:05 +0800

    代码初次提交

---
+++ src/main/java/com/youku/yklive/springboot/App.java
@@ -0,0 +1,16 @@
+package com.youku.yklive.springboot;
+
+import org.springframework.boot.SpringApplication;
+
+import com.youku.yklive.springboot.controller.SampleController;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(SampleController.class, args);
+	}
+}
